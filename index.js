0db62702b479e1576ebd4e8334dd9e4f238227b3
Ste11front <137266358+Ste11front@users.noreply.github.com>
2023-07-10 13:32:49 +0200

    a

--- index.js
+++
@@ -1,3 +0,0 @@
-//// Compito per il fine settimana:
-//1) Creare una applicazione che mostri una lista di videogiochi/film/libri(quello che volete)
-//// e che possa essere messa in ordine per nome o anno di pubblicazione
